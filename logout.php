d568c8f1a139184b9efb2053ab7bcb0c67801cf2
Irvanray21 <mirvanray@gmail.com>
2024-10-22 16:02:08 +0700

    update

--- logout.php
+++ logout.php
@@ -18,8 +18,8 @@
 </head>
 
 <body>
-   <!-- start of navbar -->
-   <nav class="navbar navbar-expand-lg custom-navbar">
+    <!-- start of navbar -->
+    <nav class="navbar navbar-expand-lg custom-navbar">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.html">
                 <img src="./media/logo.png" width="50" height="50" class="d-inline-block align-top" alt="logo">
@@ -27,9 +27,6 @@
             </a>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-2">
-                    <li class="nav-item">
-                        <a class="nav-link" href="./list_edit.php">Ubah Daftar Orang</a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./logout.php">Log-out</a>
                     </li>
@@ -45,24 +42,42 @@
             <div class="col-sm-6 mb-3 mb-sm-0">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">Update</h5>
+                        <p class="card-text">Klik di kolom ini untuk mengubah data orang yang dicari.</p>
+                        <a href="./list_edit.php" class="btn btn-primary">Ubah Data</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">Keluar</h5>
+                        <p class="card-text">Klik di kolom ini untuk keluar dari mode administrator.</p>
+                        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Log-out</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
+    !-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Apakah anda yakin ingin log-out?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+                    <button type="button" class="btn btn-primary">Ya</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- end of content -->
 
     <!-- start of footer -->
@@ -70,6 +85,19 @@
         <span>&copy;2024 Kepolisian Indonesia</span>
     </footer>
     <!-- end of footer -->
+
+    <!-- start of script -->
+    <script>
+        const myModal = document.getElementById('myModal')
+        const myInput = document.getElementById('myInput')
+
+        myModal.addEventListener('shown.bs.modal', () => {
+            myInput.focus()
+        })
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+        <!-- end of script -->
 </body>
 
 </html>l
