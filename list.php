adea477c740aa44613aa77894eda0e0cc99d253d
Irvanray21 <mirvanray@gmail.com>
2024-10-30 15:29:58 +0700

    update

--- list.php
+++ list.php
@@ -19,7 +19,8 @@ on a.stat_org = c.id_status");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Daftar Pencarian Orang</title>
     <!-- start of stylesheet -->
-    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
     <link rel="stylesheet" href="./style/style.css" />
     <link rel="stylesheet" href="./assets/fontawesome/css/all.min.css" />
     <!-- end of stylesheet -->
@@ -28,6 +29,7 @@ on a.stat_org = c.id_status");
 <body>
 
     <!-- start of navbar -->
+
     <nav class="navbar navbar-expand-lg custom-navbar">
         <div class="container-fluid">
             <a href="./index.html" class="navbar-brand">
@@ -44,6 +46,7 @@ on a.stat_org = c.id_status");
             </div>
         </div>
     </nav>
+
     <!-- end of navbar -->
 
     <!-- start of content -->
@@ -53,12 +56,10 @@ on a.stat_org = c.id_status");
 
                 <h1>Daftar Pencarian Orang</h1>
 
-                <!-- start of table  -->
                 <div class="table-responsive">
-                    <table class="table table-striped table-hover">
+                    <table id="db_dpo" class="table table-striped" style="width:100%">
                         <thead>
                             <tr>
-
                                 <th scope="col">Nama Orang</th>
                                 <th scope="col">Kejahatan</th>
                                 <th scope="col">Polsek</th>
@@ -66,7 +67,6 @@ on a.stat_org = c.id_status");
                                 <th scope="col">Waktu Hilang/Ditemukan</th>
                             </tr>
                         </thead>
-
                         <tbody>
                             <?php foreach ($data as $row): ?>
                                 <tr>
@@ -79,15 +79,13 @@ on a.stat_org = c.id_status");
 
                                 </tr>
                             <?php endforeach; ?>
-
                         </tbody>
                     </table>
                 </div>
-                <!-- end of table -->
-
             </div>
         </div>
     </div>
+
     <!-- end of content -->
 
 
@@ -102,10 +100,20 @@ on a.stat_org = c.id_status");
     <!-- end of footer -->
 
     <!-- start of script -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
-    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        <!-- DataTables JS -->
+        <script src="https://cdn.datatables.net/v/bs5/dt-2.1.8/datatables.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+    <script>
+        $('#db_dpo').DataTable();
+    </script>
     <!-- end of script -->
 </body>
 
