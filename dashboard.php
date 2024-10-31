4c45e124bee34952cab63562fef8ad174ea382f6
Irvanray21 <mirvanray@gmail.com>
2024-10-31 10:30:30 +0700

    update

--- dashboard.php
+++ dashboard.php
@@ -79,50 +79,51 @@ if (!isset($_SESSION['username'])) {
             </div>
         </div>
     </div>
-        <!-- end of dashboard -->
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Apakah anda yakin ingin log-out?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
-                        <a href="./logout.php" class="btn btn-primary" type="button">Ya</a>
-                    </div>
+    <!-- end of dashboard -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Apakah anda yakin ingin log-out?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+                    <a href="./logout.php" class="btn btn-primary" type="button">Ya</a>
                 </div>
             </div>
         </div>
-        <!-- end of content -->
+    </div>
+    <!-- end of content -->
 
-        <!-- start of footer -->
-        <footer class="bg-dark text-center text-white">
-            <!-- Copyright -->
-            <div class="text-center p-3" style="background-color:  #142c5c;">
-                &copy; 2024 Kepolisian Indonesia
-            </div>
-            <!-- Copyright -->
-        </footer>
-        <!-- end of footer -->
+    <!-- start of footer -->
+    <footer class="bg-dark text-center text-white">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color:  #142c5c;">
+            &copy; 2024 Kepolisian Indonesia
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- end of footer -->
 
-        <!-- start of script -->
-        <script>
-            const myModal = document.getElementById('myModal')
-            const myInput = document.getElementById('myInput')
+    <!-- start of script -->
+    <script>
+        const myModal = document.getElementById('myModal')
+        const myInput = document.getElementById('myInput')
 
-            myModal.addEventListener('shown.bs.modal', () => {
-                myInput.focus()
-            })
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-        <!-- end of script -->
+        myModal.addEventListener('shown.bs.modal', () => {
+            myInput.focus()
+        })
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <!-- end of script -->
 </body>
 
 </html>
